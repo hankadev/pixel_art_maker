ee124eaad40a6bdb598d80af60532008f9d206b0
Hana Klingová <hana.klingova@gmail.com>
2018-03-05 21:15:42 +0100

    added js start file
    declaring variables
    write comments what needs to be implemented

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let canvas = document.querySelector(".canvas");
+
+function makeGrid() {
+  // getting the value of height, width and color
+  let gridHeight = document.getElementById("gridHeight").value;
+  let gridWidth = document.getElementById("gridWidth").value;
+  let gridColor = document.getElementById("gridColor").value;
+  // clearing the Canvas
+
+  // appending tr and td to the canvas table
+
+  // on hover add or remove the color
+}
+
+// add event listener on submit button
+// and pass it the makeGrid function
+// do not forget the prevent default!!!!
