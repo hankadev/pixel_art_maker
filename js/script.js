81fdd5016e65a033db97aff004a0c9acb4b52e42
Hana Klingová <hana.klingova@gmail.com>
2018-03-06 11:02:39 +0100

    implemented event listeners

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ function makeGrid() {
   // converting girdHeight and gridWidth to integers
   let gridHeight = document.getElementById("gridHeight").value * 1;
   let gridWidth = document.getElementById("gridWidth").value * 1;
-  let gridColor = document.getElementById("gridColor").value;
-  // clearing the Canvas
 
   // appending tr and td to the canvas table
   for (let i = 0; i < gridHeight; i++) {
@@ -18,14 +16,36 @@ function makeGrid() {
       tableRow.appendChild(tableCell);
     }
   }
-  // on hover add or remove the color
-
 }
+
+function clearCanvas() {
+  let rowsToRemove = document.querySelectorAll("tr");
+  if (rowsToRemove.length > 1) {
+    for (let i = 0; i < rowsToRemove.length; i++) {
+      rowsToRemove[i].remove();
+    }
+  }
+}
+
+// make grid automatically when DOM is loaded
+document.addEventListener('DOMContentLoaded', makeGrid());
 
 // add event listener on submit button
 // and pass it the makeGrid function
-submitButton.addEventListener("click", function() {
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault()
+  clearCanvas();
   makeGrid();
 })
 
-// do not forget the prevent default!!!!
+// on hover add or remove the color
+canvas.addEventListener("click", function(event) {
+  let gridColor = document.getElementById("gridColor").value;
+  let cell = event.target;
+
+  if (cell.hasAttribute("style")) {
+    cell.removeAttribute("style");
+  } else {
+    cell.style.backgroundColor = gridColor;
+  }
+})
