5b782a6dcc996abc3e26f2f7ad02c147d41e63c4
Hana Klingová <hana.klingova@gmail.com>
2018-03-06 09:36:54 +0100

    adding loop for creating canvas and styling the table

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 let canvas = document.querySelector(".canvas");
+let submitButton = document.querySelector("#submitButton");
 
 function makeGrid() {
   // getting the value of height, width and color
-  let gridHeight = document.getElementById("gridHeight").value;
-  let gridWidth = document.getElementById("gridWidth").value;
+  // converting girdHeight and gridWidth to integers
+  let gridHeight = document.getElementById("gridHeight").value * 1;
+  let gridWidth = document.getElementById("gridWidth").value * 1;
   let gridColor = document.getElementById("gridColor").value;
   // clearing the Canvas
 
   // appending tr and td to the canvas table
-
+  for (let i = 0; i < gridHeight; i++) {
+    let tableRow = document.createElement("tr");
+    canvas.appendChild(tableRow);
+    for (let i = 0; i < gridWidth; i++) {
+      let tableCell = document.createElement("td");
+      tableRow.appendChild(tableCell);
+    }
+  }
   // on hover add or remove the color
+
 }
 
 // add event listener on submit button
 // and pass it the makeGrid function
+submitButton.addEventListener("click", function() {
+  makeGrid();
+})
+
 // do not forget the prevent default!!!!
